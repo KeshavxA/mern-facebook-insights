bec804e06561fb64ba7507f6b65d6ba9ec111940
Keshav Sharma <keshavsharma904517@gmail.com>
2026-08-10 21:47:26 +0530

    feat: add title tooltip to user avatar

--- src/App.jsx
+++ src/App.jsx
@@ -370,7 +370,7 @@ function App() {
         <section className="dashboard">
           <header className="dashboard-header">
             <div className="user-info">
-              <img src={user.picture} alt={`${user.name}'s avatar`} className="user-avatar" />
+              <img src={user.picture} alt={`${user.name}'s avatar`} className="user-avatar" title={`${user.name}'s Profile`} />
               <div>
                 <h2>Hello, {user.name ? user.name.split(' ')[0] : 'Guest'}</h2>
                 <span className="user-email">{user.email}</span>
